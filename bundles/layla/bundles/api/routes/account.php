6d2493fe708bb3f04a61193023765b53c9fdd29b
Koen Schmeets <k.schmeets@gmail.com>
2012-05-08 23:37:16 +0200

    Restructuring (part 3) API

---
+++ bundles/layla/bundles/api/routes/account.php
@@ -0,0 +1,148 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Account API
+|--------------------------------------------------------------------------
+|
+| 
+|
+*/
+
+// --------------------------------------------------------------
+// Get all Accounts
+// --------------------------------------------------------------
+Route::get('api/account/all', function() {
+	// Overriding default options with the "user-set" ones
+	$options = array_merge(array(
+		'offset' => 0,
+		'limit' => 20,
+		'sort_by' => 'accounts.name',
+		'order' => 'ASC'
+	), Input::all());
+
+	// Add tablename prefix to sort_by, if set
+	if(Input::has('sort_by'))
+	{
+		$options['sort_by'] = 'accounts.' . Input::get('sort_by');
+	}
+
+	// Preparing our query
+	$accounts = Account::with(array('roles', 'roles.lang', 'language'));
+
+	// Add where's to our query
+	if(array_key_exists('search', $options))
+	{
+		foreach($options['search']['columns'] as $column)
+		{
+			$accounts = $accounts->or_where($column, '~*', $options['search']['string']);
+		}
+	}
+
+	$total = $accounts->count();
+
+	// Add order_by, skip & take to our results query
+	$accounts = $accounts->order_by($options['sort_by'], $options['order'])->skip($options['offset'])->take($options['limit'])->get();
+
+	$response = array(
+		'results' => to_array($accounts),
+		'total' => $total,
+		'pages' => ceil($total / $options['limit'])
+	);
+
+	return Response::json($response);
+});
+
+// --------------------------------------------------------------
+// Get Account by id
+// --------------------------------------------------------------
+Route::get('api/account/(:num)', function($id) {
+	// Get the Account
+	$account = Account::with(array('roles', 'language'))->where_id($id)->first();
+	
+	if(is_null($account))
+	{
+		// Resource not found, return 404
+		return Response::error(404);
+	}
+
+	return Response::eloquent($account);
+});
+
+// --------------------------------------------------------------
+// Add Account
+// --------------------------------------------------------------
+Route::post('api/account', function() {
+	// We are adding an account, password is accessible and required
+	Account::$rules['password'] = 'required';
+	Account::$rules['email'] .= '|unique:accounts,email';
+	Account::$accessible[] = 'password';
+
+	// Create a new Account object
+	$account = new Account(Input::all());
+
+	// Remove empty keys (multiple's first option)
+	$role_ids = array_filter(array_flip(Input::get('role_ids')), 'strlen');
+
+	// Sync the roles (attach & detach the appropiate ones)
+	$account->roles()->sync($role_ids);
+
+	// Try to save
+	if( ! $account->save())
+	{
+		// Return 400 response with errors
+		return Response::json((array) $account->errors->messages, 400);
+	}
+	else
+	{
+		// Return the account's id
+		return Response::json($account->get_key());
+	}
+});
+
+// --------------------------------------------------------------
+// Edit Account
+// --------------------------------------------------------------
+Route::put('api/account/(:num)', function($id) {
+	// Find the account we are updating
+	$account = Account::find($id);
+
+	if(is_null($account))
+	{
+		// Resource not found, return 404
+		return Response::error(404);
+	}
+
+	// If the password is set, we allow it to be updated
+	if(Input::get('password') !== '') Account::$accessible[] = 'password';
+
+	// Fill the account with the PUT data
+	$account->fill(Input::all());
+
+	// Remove empty keys (multiple's first option)
+	$role_ids = array_filter(array_flip(Input::get('role_ids')), 'strlen');
+
+	// Sync the roles (attach & detach the appropiate ones)
+	$account->roles()->sync($role_ids);
+
+	// Try to save
+	if( ! $account->save())
+	{
+		return Response::json((array) $account->errors->messages, 400);
+	}
+});
+
+// --------------------------------------------------------------
+// Delete Account
+// --------------------------------------------------------------
+Route::delete('api/account/(:num)', function($id) {
+	// Find the account we are updating
+	$account = Account::find($id);
+
+	if(is_null($account))
+	{
+		// Resource not found, return 404
+		return Response::error(404);
+	}
+
+	$account->delete();
+});
