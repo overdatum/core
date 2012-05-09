93ae8d44580e9e39f6e2ceea1afd4265664c1959
Koen Schmeets <k.schmeets@gmail.com>
2012-05-09 02:03:17 +0200

    API starts Domain

---
+++ bundles/layla/bundles/api/bundles.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+	'layla_domain' => array(
+		'auto' => true,
+		'location' => 'layla'.DS.'bundles'.DS.'domain' 
+	),
+
+);
--- bundles/layla/bundles/api/start.php
+++ bundles/layla/bundles/api/start.php
@@ -1,5 +1,21 @@
 <?php
 
+// --------------------------------------------------------------
+// Load bundles
+// --------------------------------------------------------------
+$bundles = require __DIR__ . DS . 'bundles'.EXT;
+foreach ($bundles as $bundle => $config)
+{
+	Bundle::register($bundle, $config);
+	if($config['auto'])
+	{
+		Bundle::start($bundle);
+	}
+}
+
+// --------------------------------------------------------------
+// Load Routes
+// --------------------------------------------------------------
 require __DIR__.DS.'routes'.DS.'account'.EXT;
 require __DIR__.DS.'routes'.DS.'media'.EXT;
 require __DIR__.DS.'routes'.DS.'page'.EXT;
