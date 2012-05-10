8129f68176882db431a1ba787daef4aa6e35f87c
Koen Schmeets <k.schmeets@gmail.com>
2012-05-10 23:11:20 +0200

    Adding validation to Model

---
+++ bundles/layla/bundles/domain/libraries/model.php
@@ -0,0 +1,97 @@
+<?php namespace Layla\Domain;
+
+use Laravel\Database\Eloquent\Model as Laravel_Model;
+use Laravel\Validator;
+
+class Model extends Laravel_Model {
+
+	/**
+	 * Validation rules
+	 *
+	 * @var array
+	 */
+	public static $rules;
+
+	/**
+	 * Update a model instance in the database.
+	 *
+	 * @param  mixed  $id
+	 * @param  array  $attributes
+	 * @return int
+	 */
+	public static function update($id, $attributes)
+	{
+		$model = new static(array(), true);
+
+		if (static::$timestamps) $attributes['updated_at'] = new \DateTime;
+
+		return $model->query()->where($model->key(), '=', $id)->update($attributes);
+	}
+
+	/**
+	 * Save the model instance to the database.
+	 *
+	 * @return bool
+	 */
+	public function save()
+	{
+		if ( ! $this->dirty()) return true;
+
+		if (static::$timestamps)
+		{
+			$this->timestamp();
+		}
+
+		$this->fire_event('saving');
+
+		if(static::$rules)
+		{
+			$validator = Validator::make($this->attributes, static::$rules);
+			if( ! $validator->valid())
+			{
+				$this->errors = $validator->errors;
+
+				return false;
+			}
+		}
+
+		// If the model exists, we only need to update it in the database, and the update
+		// will be considered successful if there is one affected row returned from the
+		// fluent query instance. We'll set the where condition automatically.
+		if ($this->exists)
+		{
+			$query = $this->query()->where(static::$key, '=', $this->get_key());
+
+			$result = $query->update($this->get_dirty()) === 1;
+
+			if ($result) $this->fire_event('updated');
+		}
+
+		// If the model does not exist, we will insert the record and retrieve the last
+		// insert ID that is associated with the model. If the ID returned is numeric
+		// then we can consider the insert successful.
+		else
+		{
+			$id = $this->query()->insert_get_id($this->attributes, $this->sequence());
+
+			$this->set_key($id);
+
+			$this->exists = $result = is_numeric($this->get_key());
+
+			if ($result) $this->fire_event('created');
+		}
+
+		// After the model has been "saved", we will set the original attributes to
+		// match the current attributes so the model will not be viewed as being
+		// dirty and subsequent calls won't hit the database.
+		$this->original = $this->attributes;
+
+		if ($result)
+		{
+			$this->fire_event('saved');
+		}
+
+		return $result;
+	}
+
+}
