45d538ea1340dc7069ebcc36645be771304d3371
Koen Schmeets <k.schmeets@gmail.com>
2012-05-10 23:09:26 +0200

    Adding model_array_pluck helper

--- bundles/layla/application/helpers.php
+++ bundles/layla/application/helpers.php
@@ -15,3 +15,18 @@ function to_array($models)
 
 	return array_map(function($m) { return $m->to_array(); }, $models);
 }
+
+function model_array_pluck($models, $value, $key = null)
+{
+	$result = array();
+	if( ! is_array($models)) return $result;
+	
+	$i = 0;
+	foreach ($models as $model)
+	{
+		$result[is_null($key) ? $model->get_key() : ($key instanceof Closure ? $key($model) : ($key == '' ? $i : $model->$key))] = $value instanceof Closure ? $value($model) : $model->$value;
+		$i++;
+	}
+
+	return $result;
+}
