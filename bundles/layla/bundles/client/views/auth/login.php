b636777ebc216facc8c06933babfbed689ab7bd3
Koen Schmeets <k.schmeets@gmail.com>
2012-05-12 17:58:17 +0000

    Auth language fix

--- bundles/layla/bundles/client/views/auth/login.php
+++ bundles/layla/bundles/client/views/auth/login.php
@@ -8,9 +8,9 @@
 <div class="row">
 	<div class="span12">
 		<?= Form::open($url.'auth/login', 'PUT', array('class' => 'form-horizontal')) ?>
-			<?= Form::field('text', 'email', __('layla_client::auth.form.email'), array(Input::old('email')), array('error' => $errors->first('email'))) ?>
-			<?= Form::field('password', 'password', __('layla_client::auth.form.password'), array(), array('error' => $errors->first('password'))) ?>
-			<?= Form::actions(array(Form::submit(__('layla_client::auth.buttons.login'), array('class' => 'btn-large btn-primary')))) ?>
+			<?= Form::field('text', 'email', __('layla_client::auth.login.form.email'), array(Input::old('email')), array('error' => $errors->first('email'))) ?>
+			<?= Form::field('password', 'password', __('layla_client::auth.login.form.password'), array(), array('error' => $errors->first('password'))) ?>
+			<?= Form::actions(array(Form::submit(__('layla_client::auth.login.buttons.login'), array('class' => 'btn-large btn-primary')))) ?>
 		<?= Form::close() ?>
 	</div>
 </div>
