85a971ad6185363843ec9173770b0883f90d94f7
Koen Schmeets <k.schmeets@gmail.com>
2012-05-12 16:06:16 +0000

    Adding language for Auth

---
+++ bundles/layla/bundles/client/language/en/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+    'login' => array(
+		'title' => 'Login',
+		'form' => array(
+			'email' => 'E-mail address',
+			'password' => 'Password',
+		),
+		'buttons' => array(
+			'login' => 'Login'
+		)
+	),
+
+);
--- bundles/layla/bundles/client/views/auth/login.php
+++ bundles/layla/bundles/client/views/auth/login.php
@@ -1,16 +1,16 @@
 <div class="row">
 	<div class="span12">
 		<div class="page-header">
-			<h1>Login</h1>
+			<h1><?= __('layla_client::auth.login.title') ?></h1>
 		</div>
 	</div>
 </div>
 <div class="row">
 	<div class="span12">
 		<?= Form::open($url.'auth/login', 'PUT', array('class' => 'form-horizontal')) ?>
-			<?= Form::field('text', 'email', 'E-mail address', array(Input::old('email')), array('error' => $errors->first('email'))) ?>
-			<?= Form::field('password', 'password', 'Password', array(), array('error' => $errors->first('password'))) ?>
-			<?= Form::actions(array(Form::submit('Login', array('class' => 'btn-large btn-primary')))) ?>
+			<?= Form::field('text', 'email', __('layla_client::auth.form.email'), array(Input::old('email')), array('error' => $errors->first('email'))) ?>
+			<?= Form::field('password', 'password', __('layla_client::auth.form.password'), array(), array('error' => $errors->first('password'))) ?>
+			<?= Form::actions(array(Form::submit(__('layla_client::auth.buttons.login'), array('class' => 'btn-large btn-primary')))) ?>
 		<?= Form::close() ?>
 	</div>
 </div>
