538ab0644482ffed3ce29d61667f58bfeccb2730
Koen Schmeets <k.schmeets@gmail.com>
2012-05-17 09:21:33 +0200

    Updating link

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -8,7 +8,7 @@
 		<div class="span12">
 			<h2>A sexy CMS that knows what it wants</h2>
 			<p>
-				Your ideas are welcome! check our <a href="https://trello.com/board/layla-development/4fb2c7f48bea67b80900baa0">Trello</a> &amp; <a href="https://github.com/Rojtjo/Layla">GitHub</a> page or shoot us a mail @ <a href="mailto:we@getlayla.com">we@getlayla.com</a>
+				Your ideas are welcome! check our <a href="https://trello.com/board/layla-development/4fb2c7f48bea67b80900baa0">Trello</a> &amp; <a href="https://github.com/layla">GitHub</a> page or shoot us a mail @ <a href="mailto:we@getlayla.com">we@getlayla.com</a>
 			</p>
 		</div>
 	</div>
